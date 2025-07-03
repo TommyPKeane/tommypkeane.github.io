f871fdb8e4df482ae776c62b3ae2498abde90263
Tommy P. Keane <talk.to.tommypkeane@gmail.com>
2025-07-03 18:26:27 -0400

    [NEW] Site Basics
    - Copied from previous website, probably won't fully work
    - robots.txt trying to block webcrawlers and LLMs and all the fun stuff

---
+++ site/index.js
@@ -0,0 +1,25 @@
+import { runCommonPageUpdates } from "/modules/common.js";
+import { buildReferencesSection } from "/modules/generate-dom.js";
+import { userConfig, selectRandomGreeting } from "/modules/site-config.js";
+
+
+// Highlight.js | Setup
+window.addEventListener(
+  "load",
+  () => {
+    runCommonPageUpdates();
+    document.getElementById("header-catchphrase").innerHTML = selectRandomGreeting();
+    document.getElementById("greeting").innerHTML = `Welcome, ${userConfig.name}!`;
+    buildReferencesSection(
+      "reference-links-container",
+      {
+        "Feather Icons (SVG Icons)": "https://feathericons.com/",
+        "highlight.js (Syntax Highlighting)": "https://highlightjs.org/",
+        "Plotly (Graphing and Data Plotting)": "https://plotly.com/javascript/",
+        "Paper.js (Canvas Graphics Library)": "http://paperjs.org/",
+        "GitHub Official Logos": "https://github.com/logos",
+      }
+    );
+    return;
+  },
+);
