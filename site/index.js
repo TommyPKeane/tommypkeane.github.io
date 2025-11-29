b563f77eb2d4b8cb008722f12b3c3ae719a1e0bc
Tommy P. Keane <talk.to.tommypkeane@gmail.com>
2025-11-29 09:34:35 -0500

    [UPD] Disable Site Generation

--- site/index.js
+++ site/index.js
@@ -7,18 +7,18 @@ import { userConfig, selectRandomGreeting } from "/modules/site-config.js";
 window.addEventListener(
   "load",
   () => {
-    runCommonPageUpdates();
-    document.getElementById("header-catchphrase").innerHTML = selectRandomGreeting();
+    // runCommonPageUpdates();
+    // document.getElementById("header-catchphrase").innerHTML = selectRandomGreeting();
     // document.getElementById("greeting").innerHTML = `Welcome, ${userConfig.name}!`;
-    buildReferencesSection(
-      "reference-links-container",
-      {
-        "highlight.js (Syntax Highlighting)": "https://highlightjs.org/",
-        "Plotly (Graphing and Data Plotting)": "https://plotly.com/javascript/",
-        "Paper.js (Canvas Graphics Library)": "http://paperjs.org/",
-        "GitHub Official Logos": "https://github.com/logos",
-      }
-    );
+    // buildReferencesSection(
+    //   "reference-links-container",
+    //   {
+    //     "highlight.js (Syntax Highlighting)": "https://highlightjs.org/",
+    //     "Plotly (Graphing and Data Plotting)": "https://plotly.com/javascript/",
+    //     "Paper.js (Canvas Graphics Library)": "http://paperjs.org/",
+    //     "GitHub Official Logos": "https://github.com/logos",
+    //   }
+    // );
     return;
   },
 );
